d52f8513a37c17a174883feda36e3f726be0ce12
Akhil Kumar <akhil.se2024@gmail.com>
2024-10-27 12:56:32 +0530

    Made Responsive

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -24,7 +24,7 @@ const SignIn = () => {
   
 
   return (
-    <div className="bg-[#676179] h-screen w-full flex items-center justify-center">
+    <div className="bg-[#676179] h-screen w-full flex items-center justify-center overflow-hidden">
       <div className="flex relative items-center justify-around gap-3 bg-[#2B2738] w-[60%] h-[65%] rounded-[20px] p-3">
         <div className="relative w-full h-full rounded-[17px] hidden lg:block">
         <p className={`${index === 1 ? 'text-black' : 'text-white'} absolute font-Montserrat text-[30px] m-2 bottom-0 left-[35%]`}
@@ -37,7 +37,7 @@ const SignIn = () => {
           />
         </div>
         <div className=" w-full h-full rounded-[23px] flex flex-col items-center justify-center gap-5">
-          <h1 className="text-white text-[50px] font-Montserrat">
+          <h1 className="text-white text-center xl:text-[30px] text-[30px] font-Montserrat">
             Create an Account
           </h1>
           <p className="text-white">
